89e51edc477211c0e24b5ba868926eee54ff23b4
Mark24 <mark.zhangyoung@gmail.com>
2024-07-15 21:05:41 +0800

    feat: lazy

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+
+-- Theme
+
+-- colorscheme
+-- " There are also colorschemes for the different styles.
+-- colorscheme tokyonight-night
+-- colorscheme tokyonight-storm
+-- colorscheme tokyonight-day
+-- colorscheme tokyonight-moon
+vim.cmd[[colorscheme tokyonight-night]]
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+
+  -- Theme
+  -- https://github.com/folke/tokyonight.nvim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+    },
+    -- " There are also colorschemes for the different styles.
+    -- colorscheme tokyonight-night
+    -- colorscheme tokyonight-storm
+    -- colorscheme tokyonight-day
+    -- colorscheme tokyonight-moon
+  }
+}
